fea6c9a2b1b922f7c1f79628674c1ef8bfecce6e
Velmurugan Mohanam <Velmurugan.Mohanam@beamsuntory.com>
2022-06-15 05:23:34 -0500

    adding terraform files

--- virtualmachine.tf
+++ virtualmachine.tf
@@ -1,38 +1,38 @@
-resource "google_compute_instance" "default" {
-  name         = "test"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+# resource "google_compute_instance" "default" {
+#   name         = "test"
+#   machine_type = "e2-medium"
+#   zone         = "us-central1-a"
 
-  tags = ["foo", "bar"]
+#   tags = ["foo", "bar"]
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
+#   boot_disk {
+#     initialize_params {
+#       image = "debian-cloud/debian-9"
+#     }
+#   }
 
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
+#   // Local SSD disk
+# #   scratch_disk {
+#     interface = "SCSI"
+#   }
 
-  network_interface {
-    network = "default"
+#   network_interface {
+#     network = "default"
 
-    access_config {
-      // Ephemeral public IP
-    }
-  }
+#     access_config {
+#       // Ephemeral public IP
+#     }
+#   }
 
-  metadata = {
-    foo = "bar"
-  }
+#   metadata = {
+#     foo = "bar"
+#   }
 
-  metadata_startup_script = "echo hi > /test.txt"
+#   metadata_startup_script = "echo hi > /test.txt"
 
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
-}
+#   service_account {
+#     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+#     email  = google_service_account.default.email
+#     scopes = ["cloud-platform"]
+#   }
+# }
