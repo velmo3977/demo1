0f2cce815f2507b85d8ead7b080bf4c20e8c39aa
Velmurugan Mohanam <Velmurugan.Mohanam@beamsuntory.com>
2022-06-16 04:57:01 -0500

    VM and VPC destroy

--- Networking.tf
+++ Networking.tf
@@ -1,14 +1,14 @@
-resource "google_compute_network" "vpc_network" {
-  project                 = "demoterrform"
-  name                    = "vpc-network"
-  auto_create_subnetworks = true
-  mtu                     = 1460
-}
+# resource "google_compute_network" "vpc_network" {
+#   project                 = "demoterrform"
+#   name                    = "vpc-network"
+#   auto_create_subnetworks = true
+#   mtu                     = 1460
+# }
 
-resource "google_compute_network" "vpc_network1" {
-  project                 = "demoterrform"
-  name                    = "vpc-network1"
-  auto_create_subnetworks = false
-  #routing_mode            = regional
-  mtu                     = 1460
-}
+# resource "google_compute_network" "vpc_network1" {
+#   project                 = "demoterrform"
+#   name                    = "vpc-network1"
+#   auto_create_subnetworks = false
+#   #routing_mode            = regional
+#   mtu                     = 1460
+# }
